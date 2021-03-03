4aadca386904db4bfa144b7bb98f65b736b83191
Cionsa <daimatty@gmail.com>
2021-03-03 10:24:28 +0100

    Funzione export csv + compare score + fix vari

--- Algorithms.py
+++ Algorithms.py
@@ -13,7 +13,7 @@ from sklearn.tree import DecisionTreeRegressor
 
 def heatmap(data):
     data.corr()  # correlazione tra variabili in tabella n * n
-    hm = sns.heatmap(data.corr(), cbar=True, square=False, yticklabels=data.columns, xticklabels=data.columns)
+    sns.heatmap(data.corr(), cbar=True, square=False, yticklabels=data.columns, xticklabels=data.columns)
     plt.show()
     data.head()
 
@@ -29,54 +29,45 @@ def scaledata(data):
     y = data["cnt"]  # labels
     return X,y
 
-def rf(X_train, X_test, y_train, y_test):
+def rf(X_train, X_test, y_train, y_test, models):
     rf_model = RandomForestRegressor(n_estimators=100, max_depth=None, min_samples_split=2, random_state=0)
     rf_model.fit(X_train, y_train)
-    p_train = rf_model.predict(X_train)
-    p_test = rf_model.predict(X_test)
-    mae_train = mean_absolute_error(y_train, p_train)
-    mae_test = mean_absolute_error(y_test, p_test)
-    print("R2 score: " + str(r2_score(y_test, p_test)))
-    res = pd.DataFrame({'Actual': y_test, 'Predicted': p_test})
-    pred = pd.DataFrame(data=p_test)
-    pred.to_csv('pred.csv')
+    print("R2 score: " + str(r2_score(y_test, rf_model.predict(X_test))))
+    models.append(('rf', rf_model))
 
 
-def dt(X_train, X_test, y_train, y_test):
+def dt(X_train, X_test, y_train, y_test, models):
     dt_model = DecisionTreeRegressor()
     dt_model.fit(X_train, y_train)
-    p_train = dt_model.predict(X_train)
-    p_test = dt_model.predict(X_test)
-    mae_train = mean_absolute_error(y_train, p_train)
-    mae_test = mean_absolute_error(y_test, p_test)
-    print("R2 score: " + str(r2_score(y_test, p_test)))
-    res = pd.DataFrame({'Actual': y_test, 'Predicted': p_test})
-    pred = pd.DataFrame(data=p_test)
-    pred.to_csv('pred.csv')
+    print("R2 score: " + str(r2_score(y_test, dt_model.predict(X_test))))
+    models.append(('dt', dt_model))
 
 
-def ada(X_train, X_test, y_train, y_test):
+def ada(X_train, X_test, y_train, y_test, models):
     ada_model = AdaBoostRegressor()
     ada_model.fit(X_train, y_train)
-    p_train = ada_model.predict(X_train)
-    p_test = ada_model.predict(X_test)
-    mae_train = mean_absolute_error(y_train, p_train)
-    mae_test = mean_absolute_error(y_test, p_test)
-    print("R2 score: " + str(r2_score(y_test, p_test)))
-    res = pd.DataFrame({'Actual': y_test, 'Predicted': p_test})
-    pred = pd.DataFrame(data=p_test)
-    pred.to_csv('pred.csv')
+    print("R2 score: " + str(r2_score(y_test, ada_model.predict(X_test))))
+    models.append(('ada', ada_model))
 
 
-def gbt(X, y, X_train, X_test, y_train, y_test):
+def gbt(X, y, X_train, X_test, y_train, y_test, models):
     gb_model = GradientBoostingRegressor(loss='ls', learning_rate=0.7)
     gb_model.fit(X_train, y_train)
-    p_train = gb_model.predict(X_train)
-    p_test = gb_model.predict(X_test)
-    mae_train = mean_absolute_error(y_train, p_train)
-    mae_test = mean_absolute_error(y_test, p_test)
-    print(cross_val_score(GradientBoostingRegressor(loss='ls', learning_rate=0.7), X, y))
-    print("R2 score: " + str(r2_score(y_test, p_test)))
-    res = pd.DataFrame({'Actual': y_test, 'Predicted': p_test})
-    pred = pd.DataFrame(data=p_test)
-    pred.to_csv('pred.csv')
+    #print(cross_val_score(GradientBoostingRegressor(loss='ls', learning_rate=0.7), X, y))
+    print("R2 score: " + str(r2_score(y_test, gb_model.predict(X_test))))
+    models.append(('gbt', gb_model))
+
+def compare(models, X_test, y_test):
+    best=0
+    for name, model in models:
+        p_test=model.predict(X_test)
+        score = r2_score(y_test, p_test)
+        if(score > best):
+            best=score
+            best_name=name
+            best_test=p_test
+    return best, best_name, best_test
+        
+
+
+    
--- train.py
+++ train.py
@@ -1,28 +1,40 @@
 import pandas as pd
 import sklearn
 from sklearn.model_selection import train_test_split
-
 import Algorithms
 
 #import dataset e studio correlazione
 df = pd.read_csv("https://raw.githubusercontent.com/Cionsa/Datasets/main/hour.csv", delimiter=',')
 data = df.drop(['instant', 'registered', 'casual', 'dteday'], axis=1)
-
 Algorithms.heatmap(data)
 
+#scaling dati
 X,y = Algorithms.scaledata(data)
+
+#list predictions
+models = list()
 
 #split dataset 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 #random forest
-Algorithms.rf(X_train, X_test, y_train, y_test)
+Algorithms.rf(X_train, X_test, y_train, y_test, models)
 
 #decision tree
-Algorithms.dt(X_train, X_test, y_train, y_test)
+Algorithms.dt(X_train, X_test, y_train, y_test, models)
 
 #AdaBoost
-Algorithms.ada(X_train, X_test, y_train, y_test)
+Algorithms.ada(X_train, X_test, y_train, y_test, models)
 
 #GradientBoostingTree
-Algorithms.gbt(X, y, X_train, X_test, y_train, y_test)
+Algorithms.gbt(X, y, X_train, X_test, y_train, y_test, models)
+
+#Compare score
+best, best_name, best_test = Algorithms.compare(models, X_test, y_test)
+print(best_name, best, best_test)
+
+#Predictions to csv
+best_test_df = pd.DataFrame(data = best_test)
+best_test_df.to_csv('best_pred.csv')
+res = pd.DataFrame({'Actual': y_test, 'Predicted': best_test})
+print(res)
