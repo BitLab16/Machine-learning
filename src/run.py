8a8073eb67b2352d597a066d66d02c3b0cc9846f
Samuele Vanini <samuele.vanini@live.it>
2021-04-12 15:01:09 +0200

    fix scheduler

--- src/run.py
+++ src/run.py
@@ -3,7 +3,8 @@ import time
 from Prediction import predict
 from Training import traincode
 x, y, models, data, best, best_name, best_test, engine = traincode.traincode()
-schedule.every(5).seconds.do(predict.predictions, x, y, models, data, best, best_name, best_test, engine)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+predict.predictions(x, y, models, data, best, best_name, best_test, engine)
+#schedule.every(5).seconds.do(predict.predictions, x, y, models, data, best, best_name, best_test, engine)
+#while True:
+#    schedule.run_pending()
+#    time.sleep(1)
